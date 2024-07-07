e46e5e390070a1b03ca08130f2b4a8c97b5a6351
pengulang <r.gonzales@prostrive.io>
2024-07-07 22:38:50 +0800

    apply nodemon on docker localhost and put the networks in .env

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   REDIS_URL: process.env.REDIS_URL || "redis",
   REDIS_PORT: process.env.REDIS_PORT || 6379,
-  EVM_NETWORKS: 'sepolia', //,polygon,mainnet,sepolia
-  SOLANA_NETWORKS: 'devnet',//mainnet', //,testnet,devnet
-  EVM_SERVER_ADDRESS: '0x829c609b5EED7A5D53C684B5f8b1d3aa6DE46145',
-  SOLANA_SERVER_ADDRESS: 'ANrTqHoU4rkzUxbXW9WsDAs5w2BWMh8MUUMRGsYnca5a',
+  EVM_NETWORKS: process.env.EVM_NETWORKS || 'sepolia', //,polygon,mainnet,sepolia
+  SOLANA_NETWORKS: process.env.SOLANA_NETWORKS || 'devnet', //mainnet,testnet,devnet
+  EVM_SERVER_ADDRESS: process.env.EVM_SERVER_ADDRESS || '0x829c609b5EED7A5D53C684B5f8b1d3aa6DE46145',
+  SOLANA_SERVER_ADDRESS: process.env.SOLANA_SERVER_ADDRESS || 'ANrTqHoU4rkzUxbXW9WsDAs5w2BWMh8MUUMRGsYnca5a',
 };
 
 // // test address
